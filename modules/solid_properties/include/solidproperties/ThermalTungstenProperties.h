51af889eeb939d22506fe3282a22bbe78298b018
Eltawila <eltamm@sawtooth1.ib0.sawtooth.hpc.inl.gov>
2025-06-04 10:56:00 -0600

    change to use static constexpr, SolutionInvalididtyInterface, and fixing errors.

--- modules/solid_properties/include/solidproperties/ThermalTungstenProperties.h
+++ modules/solid_properties/include/solidproperties/ThermalTungstenProperties.h
@@ -6,6 +6,10 @@
 //*
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
+//*
+//* Data from: Milner, Justin L., Peter Karkos, and Jessica Jane Bowers. 
+//* Space Nuclear Propulsion (SNP) Material Property Handbook. 
+//* No. SNP-HDBK-0008. National Aeronautics and Space Administration, 2024.
 
 #pragma once
 
@@ -38,27 +42,27 @@ public:
 
 protected:
   // Constants used in specific heat and thermal conductivity relations
-  const Real _kN;
-  const Real _kA0;
-  const Real _kA1;
-  const Real _kA2;
-  const Real _kA3;
-  const Real _kB0;
-  const Real _kB1;
-  const Real _kB2;
-  const Real _kB3;
-  const Real _kC0;
-  const Real _kC1;
-  const Real _cN;
-  const Real _cA0;
-  const Real _cA1;
-  const Real _cA2;
-  const Real _cA3;
-  const Real _cB0;
-  const Real _cB1;
-  const Real _cB2;
-  const Real _cB3;
-  const Real _cB_2;
+  static constexpr Real _kN  = 8.7401e-01;
+  static constexpr Real _kA0  = 7.348e+05;
+  static constexpr Real _kA1  = 2.544e+01;
+  static constexpr Real _kA2  = -8.304e+03;
+  static constexpr Real _kA3  = 1.180e+06;
+  static constexpr Real _kB0  = -3.679;
+  static constexpr Real _kB1  = 1.181e+02;
+  static constexpr Real _kB2  = 5.879e+01;
+  static constexpr Real _kB3  = 2.867;
+  static constexpr Real _kC0  = -2.052e-02;
+  static constexpr Real _kC1  = 4.741e-01;
+  static constexpr Real _cN   = 3.030;
+  static constexpr Real _cA0  = 3.103e+02;
+  static constexpr Real _cA1  = -8.815;
+  static constexpr Real _cA2  = 1.295e+02;
+  static constexpr Real _cA3  = 1.874e+03;
+  static constexpr Real _cB0  = 1.301e-01;
+  static constexpr Real _cB1  = 2.225e-02;
+  static constexpr Real _cB2  = -7.224e-03;
+  static constexpr Real _cB3  = 3.539e-03;
+  static constexpr Real _cB_2  = -3.061e-04;
 };
 
 #pragma GCC diagnostic pop
